b31a0d007a08fc0f52247e5a16359ee81c5ba07b
addebski <addebski>
2020-07-12 13:00:40 +0100

    Initial commit.

---
+++ src/test/java/com/adebski/ExampleModelTest.java
@@ -0,0 +1,61 @@
+package com.adebski;
+
+import com.google.protobuf.TextFormat;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class ExampleModelTest {
+
+    @Test
+    public void testBasicOperations() throws IOException {
+        // Using generated builders to create instances of classes defined in the .protoc file
+        ExampleProtoc.AccountDetails accountDetails =
+                ExampleProtoc.AccountDetails.newBuilder()
+                        .setId("someId")
+                        .setAccountType(ExampleProtoc.AccountType.PROFESSIONAL)
+                        .build();
+        ExampleProtoc.Person person =
+                ExampleProtoc.Person.newBuilder()
+                        .setName("someName")
+                        .setSurname("someSurname")
+                        .setEmail("some_email@foo.bar")
+                        .addAccounts(accountDetails)
+                        .build();
+
+        // Taking advantage of the auto generated toString() methods
+        System.out.println("AccountDetails toString() result:");
+        System.out.println(accountDetails);
+
+        System.out.println("Person toString() result:");
+        System.out.println(person);
+
+        // Serializing using binary/compact encoding
+        ByteArrayOutputStream personAsBytesStream =
+                new ByteArrayOutputStream();
+        person.writeTo(personAsBytesStream);
+        byte[] personAsBytes = personAsBytesStream.toByteArray();
+
+        System.out.println(
+                String.format(
+                        "Serialized Person object using binary encoder it takes %d bytes in memory:",
+                        personAsBytes.length));
+        for (byte b : personAsBytes) {
+            System.out.print(String.format("%02x", b));
+        }
+        System.out.println();
+        System.out.println("original.equals(deserialized): " + ExampleProtoc.Person.parseFrom(personAsBytes).equals(person));
+
+        // Serializing using human readable TextFormat encoding
+        System.out.println();
+        String personAsTextFormat = TextFormat.printer().printToString(person);
+        System.out.println(
+                String.format(
+                        "Serialized Person object using TextFormat encoder it takes %d bytes in memory",
+                        personAsTextFormat.length()));
+        System.out.println(personAsTextFormat);
+        System.out.println("original.equals(deserialized): " + TextFormat.parse(personAsTextFormat, ExampleProtoc.Person.class).equals(person));
+
+    }
+}
---
+++ src/test/java/com/adebski/ExecutorServiceConfigurationTest.java
@@ -0,0 +1,35 @@
+package com.adebski;
+
+import com.google.protobuf.TextFormat;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class ExecutorServiceConfigurationTest {
+
+    @Test
+    public void createsExecutorServices() throws IOException {
+        ExecutorServiceProtoc.ExecutorServices executorServices = ExecutorServiceProtoc.ExecutorServices.newBuilder()
+                .addExecutorServiceConfigurations(
+                        ExecutorServiceProtoc.FixedExecutorServiceConfiguration.newBuilder()
+                                .setDaemon(true)
+                                .setNamePattern("first-test-pool-%d")
+                                .setNumberOfThreads(6)
+                                .build())
+                .addExecutorServiceConfigurations(ExecutorServiceProtoc.FixedExecutorServiceConfiguration.newBuilder()
+                        .setDaemon(true)
+                        .setNamePattern("second-test-pool-%d")
+                        .setNumberOfThreads(3)
+                        .build())
+                .build();
+
+        String exampleExecutorServiceConfiguration =
+                Files.readString(Paths.get("./example-executor-service-configuration.proto"));
+        ExecutorServiceProtoc.ExecutorServices parsedExecutorServices =
+                TextFormat.parse(exampleExecutorServiceConfiguration, ExecutorServiceProtoc.ExecutorServices.class);
+
+        System.out.println("executorServices.equals(parsedExecutorServices): " + executorServices.equals(parsedExecutorServices));
+    }
+}
